31fa9f7b85b79e9f98b44327fcef1b3283cccc4f
Erik Arvidsson <erik.arvidsson@gmail.com>
2026-02-27 15:06:08 +0100

    chore: Fix peer dependencies version and exports

--- src/react/index.ts
+++ src/react/index.ts
@@ -1,2 +1,11 @@
-export type {GetPageQuery, GetSingleQuery} from './use-rows.ts';
-export {useZeroVirtualizer} from './use-zero-virtualizer.ts';
+export type {
+  GetPageQuery,
+  GetQueryReturnType,
+  GetSingleQuery,
+} from './use-rows.ts';
+export {
+  useZeroVirtualizer,
+  type PermalinkHistoryState,
+  type UseZeroVirtualizerOptions,
+  type ZeroVirtualizerResult,
+} from './use-zero-virtualizer.ts';
