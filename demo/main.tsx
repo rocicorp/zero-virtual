2ccec899d32911e30e8f49ded8b005a5a8d1a719
Aaron Boodman <aaron@aaronboodman.com>
2026-03-12 22:12:35 -1000

    whoops remove strict mode

--- demo/main.tsx
+++ demo/main.tsx
@@ -1,5 +1,4 @@
 import {ZeroProvider} from '@rocicorp/zero/react';
-import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import {App} from './App.tsx';
 import './index.css';
@@ -9,13 +8,11 @@ const userID = import.meta.env.VITE_PUBLIC_USER_ID ?? 'anon';
 const url = new URL(window.location.href);
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <ZeroProvider
-      schema={schema}
-      userID={userID}
-      cacheURL={`${url.protocol}//${url.hostname}:4848`}
-    >
-      <App />
-    </ZeroProvider>
-  </StrictMode>,
+  <ZeroProvider
+    schema={schema}
+    userID={userID}
+    cacheURL={`${url.protocol}//${url.hostname}:4848`}
+  >
+    <App />
+  </ZeroProvider>
 );
